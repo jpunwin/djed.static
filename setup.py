74753a2d90b80547cb0e490e2b46a68e30ad4e71
Marco Strobel <mail@strobel.ch>
2015-04-09 10:22:44 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.rst')) as f:
+with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 install_requires = [
