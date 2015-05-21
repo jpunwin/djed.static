95622002e5c787e10e28acac76a266b02fc7b11d
Marco Strobel <mail@strobel.ch>
2015-05-21 22:25:41 +0200

    update classifiers and keywords

--- setup.py
+++ setup.py
@@ -13,41 +13,41 @@ install_requires = [
     'bowerstatic',
     'pyramid',
     'zope.interface',
-    ]
+]
 
 tests_require = [
     'nose',
     'pyramid_chameleon',
     'webtest',
-    ]
+]
 
 
-setup(name='djed.static',
-      version='0.4.dev0',
-      description='Integration of BowerStatic into Pyramid for managing '
-                  'static resources with Bower',
-      long_description='\n\n'.join([README, CHANGES]),
-      classifiers=[
+setup(
+    name='djed.static',
+    version='0.4.dev0',
+    description='Integration of BowerStatic into Pyramid for managing '
+                'static resources with Bower',
+    long_description='\n\n'.join([README, CHANGES]),
+    classifiers=[
+        'Development Status :: 4 - Beta',
         "Framework :: Pyramid",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: ISC License (ISCL)",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI",
-        ],
-      author='Djed developers',
-      author_email='djedproject@googlegroups.com',
-      url='https://github.com/djedproject/djed.static',
-      license='ISC License (ISCL)',
-      keywords='web pyramid pylons bower static bowerstatic',
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=install_requires,
-      extras_require={
-          'test': tests_require,
-      },
-      test_suite='nose.collector',
-      )
+    ],
+    author='Djed developers',
+    author_email='djedproject@googlegroups.com',
+    url='https://github.com/djedproject/djed.static',
+    license='ISC License (ISCL)',
+    keywords='djed pyramid pylons bower static bowerstatic',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        'test': tests_require,
+    },
+    test_suite='nose.collector',
+)
